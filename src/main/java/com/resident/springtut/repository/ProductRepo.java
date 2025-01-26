02bc526419e1df2a386a7b1317f5318c6ec92056
SILLYCAT <143728245+ACEGX25@users.noreply.github.com>
2025-01-26 22:18:34 +0530

    frontend working done

---
+++ src/main/java/com/resident/springtut/repository/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.resident.springtut.repository;
+
+import com.resident.springtut.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+}
